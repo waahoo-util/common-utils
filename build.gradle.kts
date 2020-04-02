4241a7d0667d9a48a8b126d776bba19252a51a8b
waahoo <waahoo@outlook.com>
2020-04-02 19:38:09 +0800

    first commit.

---
+++ build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+  `maven-publish`
+  kotlin("jvm") version "1.3.71"
+}
+
+group = "io.github.waahoo"
+version = "0.0.1"
+
+repositories {
+  mavenCentral()
+}
+
+dependencies {
+  implementation(kotlin("stdlib-jdk8"))
+  implementation("com.sun.mail:javax.mail:1.6.2")
+}
+
+tasks {
+  compileKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+  compileTestKotlin {
+    kotlinOptions.jvmTarget = "1.8"
+  }
+}
+
+val kotlinSourcesJar by tasks
+
+publishing {
+  publications {
+    create<MavenPublication>("maven") {
+      from(components["kotlin"])
+      artifact(kotlinSourcesJar)
+    }
+  }
+}
---
+++ src/main/kotlin/io/github/waahoo/process/ProcessHelper.kt
@@ -0,0 +1,26 @@
+package io.github.waahoo.process
+
+import java.io.BufferedReader
+
+import java.io.InputStream
+import java.io.InputStreamReader
+
+
+object ProcessHelper {
+  
+  fun run(vararg command: String) {
+    val process = ProcessBuilder(*command)
+    process.inheritIO().start().waitFor()
+  }
+  
+  fun call(vararg command: String): String {
+    val builder = ProcessBuilder(*command)
+    builder.redirectErrorStream(true)
+    val process: Process = builder.start()
+    val input: InputStream = process.inputStream
+    val reader = BufferedReader(InputStreamReader(input))
+    reader.use {
+      return it.readText()
+    }
+  }
+}
